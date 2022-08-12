600ac811de1943d2adf52461f4ce41b7aa31f77c
kashyapkbandi <kalakbandikashyap@gmail.com>
2022-08-12 19:06:37 +0530

    Adding cors

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // middleware
 // __dirname - stores path to directory
