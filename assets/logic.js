8412f7a84929059936bd40ae91b25bcfd1f2e356
kashyapkbandi <kalakbandikashyap@gmail.com>
2022-10-21 18:41:16 +0530

    make it better

--- assets/logic.js
+++ assets/logic.js
@@ -1,5 +1,21 @@
 function handleSubmit(e) {
 
     alert(e);
-    
+    var sfSessionId = document.getElementById("sessionInput").value;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    console.log("=========== XMLHttpRequest readyState:", this.readyState, ", status:", this.status);
+    if (this.readyState == 4 && this.status == 200) {
+    //action to be performed when the document is ready:
+    alert("Apex REST call succeeded");
+    }
+    };
+    xhttp.onerror = function () {
+    console.log("=========== XMLHttpRequest readyState:", this.readyState, ", status:", this.status);
+    alert("Apex REST call failed");
+    };
+    xhttp.open("GET", "https://sfdxworks-dev-ed.my.salesforce.com/services/data/v52.0/search/?q=FIND%20%7Bandy%7D", true);
+    xhttp.setRequestHeader("Authorization", "Bearer "+sfSessionId);
+    xhttp.send();
 }
