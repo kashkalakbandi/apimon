308fc903b9bfd4558a5e675ce7318e437f0c709f
kashyapkbandi <kalakbandikashyap@gmail.com>
2022-10-21 18:46:45 +0530

    make it better

--- assets/logic.js
+++ assets/logic.js
@@ -3,21 +3,33 @@ e.preventDefault();
 
     var sfSessionId = document.getElementById("sessionInput").value;
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-    console.log("=========== XMLHttpRequest readyState:", this.readyState, ", status:", this.status);
-    if (this.readyState == 4 && this.status == 200) {
-    //action to be performed when the document is ready:
-    alert("Apex REST call succeeded");
-    }
-    };
-    xhttp.onerror = function () {
-    console.log("=========== XMLHttpRequest readyState:", this.readyState, ", status:", this.status);
-    alert("Apex REST call failed");
-    };
-    xhttp.open("GET", "https://sfdxworks-dev-ed.my.salesforce.com/services/data/v52.0/search/?q=FIND%20%7Bandy%7D", true);
-    xhttp.setRequestHeader("Authorization", "Bearer "+sfSessionId);
-    xhttp.send();
+        fetch('https://sfdxworks-dev-ed.my.salesforce.com/services/data/v52.0/search/?q=FIND%20%7Bandy%7D',
+       {method: 'POST',
+       headers: {
+        'Content-Type': 'application/json',
+        'Authorization':'Bearer '+sfSessionId
+      }
+        }
+        )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+
+
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function() {
+    // console.log("=========== XMLHttpRequest readyState:", this.readyState, ", status:", this.status);
+    // if (this.readyState == 4 && this.status == 200) {
+    // //action to be performed when the document is ready:
+    // alert("Apex REST call succeeded");
+    // }
+    // };
+    // xhttp.onerror = function () {
+    // console.log("=========== XMLHttpRequest readyState:", this.readyState, ", status:", this.status);
+    // alert("Apex REST call failed");
+    // };
+    // xhttp.open("GET", "https://sfdxworks-dev-ed.my.salesforce.com/services/data/v52.0/search/?q=FIND%20%7Bandy%7D", true);
+    // xhttp.setRequestHeader("Authorization", "Bearer "+sfSessionId);
+    // xhttp.send();
 
 
 }
